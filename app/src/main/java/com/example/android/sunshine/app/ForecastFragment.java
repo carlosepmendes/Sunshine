01f3a73847dbe5a902f641e23b25f93ac40ead27
Carlos Mendes <carlosepmendes@gmail.com>
2015-06-09 10:21:10 +0000

    Refactored ForecastFragment

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -24,9 +24,9 @@ import java.util.List;
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment {
+public class ForecastFragment extends Fragment {
 
-    public MainActivityFragment() {
+    public ForecastFragment() {
     }
 
     ArrayAdapter<String> mForecastAdapter;
